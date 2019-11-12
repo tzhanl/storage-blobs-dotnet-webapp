e5bdc80e42d49fd3692ca3d2ae0514a627d80076
wantedfast <inaccuarcy@gmail.com>
2019-11-12 13:55:28 +0800

    add

--- README.md
+++ README.md
@@ -15,23 +15,25 @@ This sample application creates a web photo gallery that allows you to host and 
 
 ## Technologies used
 - ASP.NET MVC 5
-- .NET 4.5
 - Azure Storage emulator
 - Azure Web Apps
 - Azure Storage
 
-Azure Blob Storage Photo Gallery Web Application using ASP.NET MVC 5. The sample uses the .NET 4.5 asynchronous programming model to demonstrate how to call the Storage Service using the Storage .NET client library's asynchronous APIs.
+Azure Blob Storage Photo Gallery Web Application using ASP.NET MVC 5. The sample uses asynchronous programming model to demonstrate how to call the Storage Service using the Storage .NET client library's asynchronous APIs.
 
 ## Running this sample
 1. Before you can run this sample, you must have the following prerequisites:
 	- The Azure Storage Emulator, which you can download [here](https://go.microsoft.com/fwlink/?linkid=717179&clcid=0x409). You can also read more about [Using the Azure Storage emulator for development](https://docs.microsoft.com/en-us/azure/storage/common/storage-use-emulator).
-	- Visual Studio 2015 or Visual Studio 2017.
+	- Visual Studio 2017 or Visual Studio 2019.
 
 2. Open the Azure Storage emulator. Once the emulator is running it will be able to process the images from the application.
 
-3. Clone this repository using Git for Windows (http://www.git-scm.com/), or download the zip file.
+3. Clone this repository using Git :
+```cmd
+git clone https://github.com/tzhanl/storage-blobs-dotnet-webapp.git
+```	
 
-4. From Visual Studio, open the **WebApp-Storage-DotNet.sln** file from the root directory.
+4. Switch to the appropriate folder. Navigate to your directory where the project file (.csproj) resides and open it with Visual Studio.
 
 5. In Visual Studio Build menu, select **Build Solution** (or Press F6).
 
@@ -41,11 +43,11 @@ Azure Blob Storage Photo Gallery Web Application using ASP.NET MVC 5. The sample
 
 1. To make the sample work in the cloud, you must replace the connection string with the values of an active Azure Storage Account. If you don't have an account, refer to the [Create a Storage Account](https://azure.microsoft.com/en-us/documentation/articles/storage-create-storage-account/) article.
 
-2. Retrieve the STORAGE ACCOUNT NAME and PRIMARY ACCESS KEY (or SECONDARY ACCESS KEY) values from the Keys blade of your Storage account in the Azure Preview portal. For more information on obtaining keys for your Storage account refer to [View, copy, and regenerate storage access keys](https://azure.microsoft.com/en-us/documentation/articles/storage-create-storage-account/#view-copy-and-regenerate-storage-access-keys)
+2. Click Storage Account in the Azure Portal to open it. Select **Settings** > **Access keys** > **Primary Key**, copy the associated **Connection string** to the clipboard, then paste it into a text editor for later use.
 
 2. In the **Web.config** file, located in the project root, find the **StorageConnectionString** app setting and replace the placeholder values with the values obtained for your account.
 
-  <add key="StorageConnectionString" value="DefaultEndpointsProtocol=https;AccountName=[Enter Your Storage AccountName];AccountKey=[Enter Your Storage AccountKey]" />
+  <add key="StorageConnectionString" value="[Enter Your Storage Connection string]" />
 
 3. In Visual Studio Solution Explorer, right-click on the project name and select **Publish...**
 
@@ -59,11 +61,20 @@ Azure Blob Storage Photo Gallery Web Application using ASP.NET MVC 5. The sample
 
 For additional ways to deploy this web application to Azure, please refer to the [Deploy a web app in Azure App Service](https://azure.microsoft.com/en-us/documentation/articles/web-sites-deploy/) article which includes information on using Azure Resource Manager (ARM) Templates, Git, MsBuild, PowerShell, Web Deploy, and many more.
 
+## This Quickstart shows how to do some basic operations of Storage Blobs.
+- Create a container.
+- Upload images to storage blob.
+- List block blobs.
+- Delete blobs.
+
+## Folders Introduction
+You will find the following folders: WebApp-Storage-DotNet-v3, which references the [Microsoft.Azure.Storage.Blob](https://www.nuget.org/packages/Microsoft.Azure.Storage.Blob/) SDK and WebApp-Storage-DotNet-v12, which uses the [Azure.Storage.Blobs](https://www.nuget.org/packages/Azure.Storage.Blobs/) version of the SDK.
+
 ## About the code
 The code included in this sample is meant to be a quick start sample for learning about Azure Web Apps and Azure Storage. It is not intended to be a set of best practices on how to build scalable enterprise grade web applications.
 
 ## More information
-- [What is a Storage Account](http://azure.microsoft.com/en-us/documentation/articles/storage-whatis-account/)
+- [What is Azure Blob Storage](https://docs.microsoft.com/en-us/azure/storage/blobs/storage-blobs-overview)
 - [Getting Started with Blobs](http://azure.microsoft.com/en-us/documentation/articles/storage-dotnet-how-to-use-blobs/)
 - [Blob Service Concepts](http://msdn.microsoft.com/en-us/library/dd179376.aspx)
 - [Blob Service REST API](http://msdn.microsoft.com/en-us/library/dd135733.aspx)
---
+++ WebApp-Storage-DotNet-v12/Controllers/HomeController.cs
@@ -0,0 +1,209 @@
+﻿//---------------------------------------------------------------------------------- 
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+// 
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,  
+// EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES  
+// OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE. 
+//---------------------------------------------------------------------------------- 
+// The example companies, organizations, products, domain names, 
+// e-mail addresses, logos, people, places, and events depicted 
+// herein are fictitious.  No association with any real company, 
+// organization, product, domain name, email address, logo, person, 
+// places, or events is intended or should be inferred. 
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace WebApp.Storage.DotNet.Controllers
+{
+    /// <summary> 
+    /// Azure Blob Storage Photo Gallery - Demonstrates how to use the Blob Storage service.  
+    /// Blob storage stores unstructured data such as text, binary data, documents or media files.  
+    /// Blobs can be accessed from anywhere in the world via HTTP or HTTPS. 
+    /// </summary> 
+    public class HomeController : Controller
+    {
+        const string BlobContainerName = "webappstoragedotnet-imagecontainer";
+        BlobContainerClient blobContainer;
+        static bool containerCreated = false;
+
+        public HomeController()
+        {
+            if (!containerCreated)
+            {
+                string connectionString = ConfigurationManager.AppSettings["StorageConnectionString"].ToString();
+
+                // Create a client to interact with blob service.
+                BlobServiceClient client = new BlobServiceClient(connectionString);
+
+                // Create a container named "webappstoragedotnet-imagecontainer" in this storage account first if you want interact with truly Azure service.
+                blobContainer = client.GetBlobContainerClient(BlobContainerName);
+                blobContainer.CreateIfNotExists();
+
+                // To view the uploaded blob in a browser, you have two options. The first option is to use a Shared Access Signature (SAS) token to delegate  
+                // access to the resource. The second approach is to set permissions to allow public access to blobs in this container.Then you can view the image.  
+                blobContainer.SetAccessPolicy(PublicAccessType.Blob);
+
+                containerCreated = true;
+            }
+        }
+
+        #region controller
+        /// <summary> 
+        /// ActionResult Index() 
+        /// Documentation References:  
+        /// - What is a Storage Account: https://docs.microsoft.com/en-us/azure/storage/common/storage-quickstart-create-account?toc=%2Fazure%2Fstorage%2Fblobs%2Ftoc.json&tabs=azure-portal
+        /// - Create a Storage Account: https://docs.microsoft.com/en-us/azure/storage/common/storage-quickstart-create-account?tabs=azure-portal#create-a-storage-account-1
+        /// - Create a Storage Container: https://azure.microsoft.com/en-us/documentation/articles/storage-dotnet-how-to-use-blobs/#create-a-container
+        /// - List all Blobs in a Storage Container: https://azure.microsoft.com/en-us/documentation/articles/storage-dotnet-how-to-use-blobs/#list-the-blobs-in-a-container
+        /// </summary> 
+        public async Task<ActionResult> Index()
+        {
+            try
+            {
+                InitClient();
+                Uri prefix = blobContainer.Uri;
+
+                // Gets all Block Blobs in the BlobContainerName and passes them to teh view
+                List<Uri> allBlobs = new List<Uri>();
+                await foreach (BlobItem blob in blobContainer.GetBlobsAsync())
+                {
+                    if (blob.Properties.BlobType == BlobType.Block)
+                    {
+                        allBlobs.Add(new Uri($"{prefix}/{blob.Name}"));
+                    }
+                }
+
+                return View(allBlobs);
+            }
+            catch (Exception ex)
+            {
+                ViewData["message"] = ex.Message;
+                ViewData["trace"] = ex.StackTrace;
+                return View("Error");
+            }
+        }
+
+        /// <summary> 
+        /// Task<ActionResult> UploadAsync() 
+        /// </summary> 
+        [HttpPost]
+        public async Task<ActionResult> UploadAsync()
+        {
+            try
+            {
+                InitClient();
+
+                HttpFileCollectionBase files = Request.Files;
+                int fileCount = files.Count;
+
+                if (fileCount > 0)
+                {
+                    for (int i = 0; i < fileCount; i++)
+                    {
+                        BlobClient blob = blobContainer.GetBlobClient(GetRandomBlobName(files[i].FileName));
+                        await blob.UploadAsync(files[i].InputStream);
+                    }
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ViewData["message"] = ex.Message;
+                ViewData["trace"] = ex.StackTrace;
+                return View("Error");
+            }
+        }
+
+        /// <summary> 
+        /// Task<ActionResult> DeleteImage(string name) 
+        /// </summary> 
+        [HttpPost]
+        public async Task<ActionResult> DeleteImage(string name)
+        {
+            try
+            {
+                InitClient();
+
+                Uri uri = new Uri(name);
+                string filename = Path.GetFileName(uri.LocalPath);
+                var blob = blobContainer.GetBlobClient(filename);
+                await blob.DeleteAsync();
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ViewData["message"] = ex.Message;
+                ViewData["trace"] = ex.StackTrace;
+                return View("Error");
+            }
+        }
+
+        /// <summary> 
+        /// Task<ActionResult> DeleteAll(string name) 
+        /// </summary> 
+        [HttpPost]
+        public async Task<ActionResult> DeleteAll()
+        {
+            try
+            {
+                InitClient();
+
+                await foreach (BlobItem blob in blobContainer.GetBlobsAsync())
+                {
+                    if (blob.Properties.BlobType == BlobType.Block)
+                    {
+                        await blobContainer.DeleteBlobAsync(blob.Name);
+                    }
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ViewData["message"] = ex.Message;
+                ViewData["trace"] = ex.StackTrace;
+                return View("Error");
+            }
+        }
+        #endregion
+
+        #region helpers
+        /// <summary> 
+        /// void InitClient()
+        /// string GetRandomBlobName(string filename): Generates a unique random file name to be uploaded  
+        /// </summary> 
+        private string GetRandomBlobName(string filename)
+        {
+            string ext = Path.GetExtension(filename);
+            return string.Format("{0:10}_{1}{2}", DateTime.Now.Ticks, Guid.NewGuid(), ext);
+        }
+
+        /// <summary>
+        /// Initialize a blobContainer client 
+        /// </summary>
+        private void InitClient()
+        {
+            if (blobContainer == null && containerCreated)
+            {
+                string connectionString = ConfigurationManager.AppSettings["StorageConnectionString"].ToString();
+
+                // Create a client to interact with blob service.
+                BlobServiceClient client = new BlobServiceClient(connectionString);
+
+                // Get the exist container named "webappstoragedotnet-imagecontainer" from this storage account.
+                blobContainer = client.GetBlobContainerClient(BlobContainerName);
+            }
+        }
+        #endregion
+    }
+}
